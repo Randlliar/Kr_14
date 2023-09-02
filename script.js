2ff28aea4d57ef102ef4d02090dd97d1e0e7dfef
Igor Sharkevich <sharkevich.i@inbox.ru>
2023-09-02 16:55:59 +0300

    hotfix

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ function logInPage() {
     inputBlock[4].remove();
     $('.checkbox').remove();
     console.log(localStorage)
-    // $('.input-value').val('');
+    $('.input-value').val('');
     title.val('Log in to the system');
     account.text('Registration');
     signUpBtn.text('Sign In');
@@ -154,49 +154,9 @@ submitBtn.on('click', function () {
   logInPage();
 });
 
-// account.on('click', function () {
-// });
-account.on('click', function () {
-  logIn()
-})
 
-function logIn (){
 
-    logInPage();
-}
-
-function reload (){
   account.on('click', function () {
     location.reload();
   })
-}
 
-
-
-// function logInPage() {
-//   console.log(document.getElementsByClassName('input-block')[4]);
-//
-//   document.getElementById('title').innerText = "Log in to the system";
-//   document.getElementsByClassName('input-block')[4].remove();
-//   document.getElementsByClassName('input-block')[2].remove();
-//   document.getElementsByClassName('input-block')[0].remove();
-//   document.getElementById('sign-up-btn').innerText = "Log In";
-//
-//   document.getElementsByClassName('checkbox')[0].remove();
-//   document.getElementsByClassName('account')[0].remove();
-//   popup.style.display = 'none';
-//   userName.value = "";
-//   password.value = "";
-//
-//   signUpBtn.onclick = function () {
-//     if (!password.value) {
-//       alert("Неверно введен пароль");
-//       return;
-//     }
-//     if (!userName.value) {
-//       alert("Неверно введено имя пользователя");
-//       return;
-//     }
-//     alert(`Добро пожаловать, ${userName.value}!`)
-//   }
-// }
